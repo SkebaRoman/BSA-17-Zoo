7e425b0c7dc7b4f4391034c2a5d15a865579e750
Restart <roman-skeba@mail.ru>
2017-05-24 15:58:32 +0300

    added class Lion

---
+++ ZooClassLibrary/Animals/Lion.cs
@@ -0,0 +1,50 @@
+﻿using ZooClassLibrary.UserException;
+
+namespace ZooClassLibrary
+{
+    public class Lion : Animal
+    {
+        public override int CurrentHealth { get; set; } = 5;
+        public override int MaxHealth { get; protected set; } = 5;
+        public override string Name { get; set; }
+        public override State CurrentState { get; set; } = State.Sated;
+        public Lion(string name) : base(name) { }
+        public override string ToString()
+        {
+            return string.Format("This is Lion called: " + Name + ", with state: " + CurrentState + " and health: " + CurrentHealth);
+        }
+        public override void Eat()
+        {
+            CurrentState = State.Sated;
+        }
+        public override void Cure()
+        {
+            if (CurrentHealth + 1 <= MaxHealth)
+            {
+                CurrentHealth += 1;
+            }
+            else
+            {
+                throw new AnimalMaxHealthException();
+            }
+        }
+        public override void ChangeState()
+        {
+            if (CurrentState == State.Sick)
+            {
+                if (CurrentHealth - 1 > 0)
+                {
+                    CurrentHealth -= 1;
+                }
+                else
+                {
+                    CurrentState = State.Dead;
+                }
+            }
+            else if (CurrentState != State.Dead)
+            {
+                CurrentState -= 1;
+            }
+        }
+    }
+}
