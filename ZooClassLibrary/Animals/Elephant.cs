13d05e23dfc93ed1bd1dab396e78514433681c3d
Restart <roman-skeba@mail.ru>
2017-05-24 15:58:12 +0300

    added class Elephant

---
+++ ZooClassLibrary/Animals/Elephant.cs
@@ -0,0 +1,50 @@
+﻿using ZooClassLibrary.UserException;
+
+namespace ZooClassLibrary
+{
+    public class Elephant : Animal
+    {
+        public override int CurrentHealth { get; set; } = 7;
+        public override int MaxHealth { get; protected set; } = 7;
+        public override string Name { get; set; }
+        public override State CurrentState { get; set; } = State.Sated;
+        public Elephant(string name) : base(name) { }
+        public override void Eat()
+        {
+            CurrentState = State.Sated;
+        }
+        public override void Cure()
+        {
+            if (CurrentHealth + 1 <= MaxHealth)
+            {
+                CurrentHealth += 1;
+            }
+            else
+            {
+                throw new AnimalMaxHealthException();
+            }
+        }
+        public override void ChangeState()
+        {
+            if (CurrentState == State.Sick)
+            {
+                if (CurrentHealth - 1 > 0)
+                {
+                    CurrentHealth -= 1;
+                }
+                else
+                {
+                    CurrentState = State.Dead;
+                }
+            }
+            else if (CurrentState != State.Dead)
+            {
+                CurrentState -= 1;
+            }
+        }
+        public override string ToString()
+        {
+            return string.Format("This is Elephant called: " + Name + ", with state: " + CurrentState + " and health: " + CurrentHealth);
+        }
+    }
+}
