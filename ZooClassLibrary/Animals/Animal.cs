836f43447001802ad915bec4cbaa4c04f393da5e
Restart <roman-skeba@mail.ru>
2017-05-24 15:57:44 +0300

    added abstract class Animal

---
+++ ZooClassLibrary/Animals/Animal.cs
@@ -0,0 +1,20 @@
+﻿namespace ZooClassLibrary
+{
+    public enum State { Dead, Sick, Hungry, Sated }
+
+    public abstract class Animal
+    {
+        public abstract string Name { get; set; }
+        public abstract int CurrentHealth { get; set; }
+        public abstract int MaxHealth { get; protected set; }
+        public abstract State CurrentState { get; set; }
+        public Animal(string name)
+        {
+            Name = name;
+        }
+        public abstract void Eat();
+        public abstract void Cure();
+        public abstract void ChangeState();
+        public abstract override string ToString();
+    }
+}
